69a85fe520e8ca3defdc5d12591332cfed144212
ajwebee1326 <ajay.kumar@webeesocial.com>
2024-05-20 10:50:47 +0530

    worked on demo migration and connected backedn page

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Demo;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
 {
-    //
+    
+
+    public function index()
+    {
+
+        $demos = Demo::all();
+        return view('admin.demo.index', compact('demos'));
+
+    }
+
 }
